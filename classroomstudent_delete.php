fb60b08f4dd9255209582aaf6f9c0033e36395fd
markbrand1337 <54582066+markbrand1337@users.noreply.github.com>
2020-04-16 18:36:56 +0700

    Add files via upload

--- classroomstudent_delete.php
+++ classroomstudent_delete.php
@@ -4,10 +4,13 @@ include_once("controller/c_classroomstudent.php");
 
 if(isset($_GET['id']) 
 	&&filter_var($_GET['id'],FILTER_VALIDATE_INT,array('min_range'=>1) ) 
+	&& isset($_GET['id2']) 
+	&&filter_var($_GET['id2'],FILTER_VALIDATE_INT,array('min_range'=>1) ) 
 	){
 				$id=$_GET['id'];
+			$id2=$_GET['id2'];
 				$controller = new c_ClassroomStudent();
-				$controller->DeleteClassroomStudent($id);
+				$controller->DeleteClassroomStudent($id2,$id);
 }
 
 
---
+++ report.php
@@ -0,0 +1,9 @@
+<?php
+
+// require_once("controller/c_user.php");
+// $controller = new c_User();
+require_once("controller/c_report.php");
+$controller = new c_Report();
+$controller->getReport();
+
+?>
